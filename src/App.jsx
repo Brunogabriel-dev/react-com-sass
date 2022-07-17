cc0d977f8551899ad579fc6c518252fd9e39d1b4
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-16 21:11:27 -0300

    Inclusão de texto

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
-function App() {
+import "./styles/components/app.sass";
 
+function App() {
   return (
-    <div className="App">
-      <h1>React + SASS</h1>
+    <div id="portfolio">
+      <h1>Bruno Gabriel</h1>
     </div>
   )
 }
 
-export default App
+export default App;
