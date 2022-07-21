cea07e5aed3d611864feb95564e0affda5e84377
Bruno Gabriel Santos <brunogbl1sts@gmail.com>
2022-07-20 22:34:30 -0300

    Inclusão de texto

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,5 +1,13 @@
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+  <aside id="sidebar">
+    <p>imagem</p>
+    <p className="title">Desenvolvedor</p>
+    <p>redes sociais</p>
+    <p>infomações de contato</p>
+    <a href="" className="btn">Dowload currículo</a>
+  </aside>
+ );
 };
 
 export default Sidebar;
